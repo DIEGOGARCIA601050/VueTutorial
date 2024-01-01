0c84c84e42fd9daee608784e38ffa8464177b269
DiegoGRoldán <ekrnal9@gmail.com>
2023-12-31 19:57:02 -0600

    añadimos contador y lógica en el html, en el .vue no

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,17 @@
 import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import HelloName from './components/HelloName.vue';
+import Conversor from './components/Conversor.vue';
 const numbers = ["peru", "red", "blue"]
 let count = ref(3)
 const name = 'Vue.js'
+let active = true
+const Usuario = null
+const resultado = false
 
 function greet(event) {
   console.log(`Hello ${name}!`)
-  count--
-  // `event` is the native DOM event
-  if (event) {
-    count = count - 1
-    alert(event.target.tagName)
-  }
+  active = !active
 }
 </script>
 
@@ -22,6 +21,9 @@ function greet(event) {
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <Conversor DivisaDeOrigen="pesos" DivisaACambiar="dolares" />
+      <Conversor DivisaDeOrigen="pesos" DivisaACambiar="euros" />
+      <Conversor DivisaDeOrigen="Dolares" DivisaACambiar="euros" />
       <HelloWorld msg="You did it!" msg2="You've successfully create a project with vite + vue" class="Initial" />
       <HelloWorld msg="Vue" msg2=" version 3" />
       <HelloWorld msg="Hola edmundo " msg2="Si funcionó" />
@@ -34,11 +36,17 @@ function greet(event) {
     <HelloName nombresito="Luis"></HelloName>
     <HelloName nombresito="Abraham"></HelloName>
     <!-- `greet` is the name of the method defined above -->
-    <button @click="greet">Greet</button>
-    <button @click="count = count - 1"></button>
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button class="event" @click="greet">Greet</button>
+    <button class="button-count" @click="count = count - 1"> - </button>
+    <p class="count">{{ count }}</p>
+    <button class="button-count" type="button" @click="count++"> + </button>
   
-    <p>{{ count }}</p>
+    
+    <p>{{ Usuario ? 'resultado' : 'null' }}</p>
+    <p v-if="!resultado">null</p>
+    <p v-else>{{ count-- }}</p>
+    <p v-show="active">Activo</p>
+    <p v-show="count">Activo</p>
   </header>
 </template>
 
---
+++ src/components/Conversor.vue
@@ -0,0 +1,51 @@
+<script setup>
+    defineProps({
+        DivisaDeOrigen: {
+            type: String,
+            Request: true
+        },
+        DivisaACambiar:{
+            type: String,
+            Request: true
+        }
+    });
+
+      
+</script>
+
+<template>
+    <section class="divisa">
+          <h1>Convertir de {{ DivisaDeOrigen }} a {{ DivisaACambiar }}</h1>
+          <form action="f" id='formulario'>
+            <label for="dollars">
+              Introduzca la cantidad de {{ DivisaDeOrigen }} que desea cambiar: </label
+            ><br />
+            <input type="number" id="dollars" name="dollars" />
+            <input type="submit" contenteditable="enviar" />
+          </form>
+        </section>
+        <p>{{ resultado }}</p>
+</template>
+
+<style>
+  section {
+    background-color: rgb(90, 84, 187);
+    color: black;
+    display: inline-block;
+    padding-top: 10px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+  }
+  .divisa {
+    float: left;
+    flex-basis: 100px;
+    flex-grow: 1;
+  }
+
+  .divisa h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
